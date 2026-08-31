b459bb7e2288c3d8f7746fa1b7ebfb5a9310bb92
epilepticseizureee <business.egor.sg@gmail.com>
2026-08-31 21:43:43 +0300

    fix(migration): fix array subscript syntax in 0185 migration

--- deploy/migrations/0185_authorizations_ip_backfill.up.sql
+++ deploy/migrations/0185_authorizations_ip_backfill.up.sql
@@ -1,9 +1,9 @@
 UPDATE public.authorizations
 SET ip = (
-  ARRAY[
+  (ARRAY[
     '178.62', '95.173', '188.130', '213.87', '46.138', '91.242', 
     '5.165', '37.144', '176.59', '89.178', '94.25', '185.77', '77.82', '31.173'
-  ][1 + abs(hashtext(auth_key_id::text || user_id::text)) % 14]
+  ])[1 + abs(hashtext(auth_key_id::text || user_id::text)) % 14]
   || '.' || (1 + abs(hashtext(user_id::text || 'c')) % 254)::text
   || '.' || (1 + abs(hashtext(auth_key_id::text || 'd')) % 254)::text
 )
